d1bb0ddd2f7456074d9d71530c71e4f6e98d67c7
Caleb Ellis <caleb.ellis@canonical.com>
2018-08-17 17:56:26 +0100

    First pass at building bar charts from dummy data

--- static/js/userReportCharts.js
+++ static/js/userReportCharts.js
@@ -8,8 +8,10 @@ function createPieChart(selector, data, donutRadius, colors) {
   var height = svg.attr('height');
   var width = svg.attr('width');
   var radius = Math.min(height, width) / 2;
-  var g = svg.append("g").attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
   var ordinalColors = d3.scaleOrdinal(colors);
+  var g = svg.append("g")
+    .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')')
+    .attr('class', 'p-pie-chart');
 
   // Generate the pie
   var pie = d3.pie();
@@ -20,11 +22,11 @@ function createPieChart(selector, data, donutRadius, colors) {
     .outerRadius(radius);
 
   // Generate the groups
-  var arcs = g.selectAll('arc')
+  var arcs = g.selectAll('.p-pie-chart__wedge')
     .data(pie(data.dataset))
     .enter()
     .append('g')
-    .attr('class', 'arc');
+    .attr('class', 'p-pie-chart__wedge');
 
   // Draw arc paths
   arcs.append('path')
@@ -46,9 +48,68 @@ function setInnerHTML(target, value) {
   document.querySelector(target).innerHTML = value;
 }
 
+function createBarChart(selector, data, margin) {
+  // Set parameter defaults
+  var margin = typeof margin !== 'undefined' ? margin : { top: 20, right: 40, bottom: 20, left: 40 };
+
+  // Orientate svg and give it class name
+  var svg = d3.select(selector);
+  var width = +svg.attr("width") - margin.left - margin.right;
+  var height = +svg.attr("height") - margin.top - margin.bottom;
+  var g = svg.append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+    .attr("class", "p-bar-chart");
+  
+  // Helper function to turn raw count into percentage of total count
+  var calcPercentage = function(d) {
+    var sum = d3.sum(data.dataset, function(d) { return d.value });
+    var percentage = d.value * 100 / sum;
+
+    return percentage;
+  }
+
+  // Set axis domains and range
+  var x = d3.scaleBand()
+    .rangeRound([0, width])
+    .padding(0.1)
+    .domain(data.dataset.map(function(d) { return d.label }));
+  
+  var y = d3.scaleLinear()
+    .rangeRound([height, 0])
+    .domain([0, Math.ceil(d3.max(data.dataset, function(d) { return calcPercentage(d) }))]);
+
+  // Generate axes
+  g.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x))
+  
+  g.append("g")
+    .call(d3.axisLeft(y).tickFormat(function(d) { return d + '%' }))
+
+  // Generate bars
+  g.selectAll(".p-bar-chart__bar")
+    .data(data.dataset)
+    .enter()
+    .append("rect")
+    .attr("class", "p-bar-chart__bar")
+    .attr("x", function(d) { return x(d.label) })
+    .attr("y", function(d) { return y(calcPercentage(d)) })
+    .attr("width", x.bandwidth())
+    .attr("height", function(d) { return height - y(calcPercentage(d)) })
+}
+
 createPieChart('#opt-in', userReportData.optIn, 15);
 createPieChart('#real-or-virtual', userReportData.realOrVirtual, 15);
 createPieChart('#firmware', userReportData.firmware, 15);
+
+createBarChart('#popular-screen-sizes', userReportData.screenSizes);
+createBarChart('#physical-disk', userReportData.physicalDisk, { top: 20, right: 60, bottom: 20, left: 60 });
+createBarChart('#number-of-cpus', userReportData.CPUs);
+createBarChart('#size-of-ram', userReportData.RAM);
+createBarChart('#pixel-density', userReportData.pixelDensity);
+createBarChart('#partition-type', userReportData.partitionType);
+createBarChart('#partition-size', userReportData.partitionSize);
+createBarChart('#partition-number', userReportData.partitionNum);
 
 setInnerHTML('#os-architecture', userReportData.osArchitecture.dataset[0] + '%');
 setInnerHTML('#display-server', userReportData.displayServer.dataset[0] + '%');
--- static/js/userReportData.js
+++ static/js/userReportData.js
@@ -34,4 +34,254 @@ var userReportData = {
     labels: ['One', 'Two', 'Three'],
     dataset: [87, 10, 3],
   },
+  screenSizes: {
+    title: 'Popular screen sizes',
+    dataset: [
+      {
+        label: '800x600',
+        value: 256110,
+      }, {
+        label: '1024x768',
+        value: 86040,
+      }, {
+        label: '1152x768',
+        value: 46550,
+      }, {
+        label: '1152x864',
+        value: 25880,
+      }, {
+        label: '1280x1024',
+        value: 80030,
+      }, {
+        label: '1280x800',
+        value: 50430,
+      }, {
+        label: '1360x768',
+        value: 36250,
+      }, {
+        label: '1366x768',
+        value: 582440,
+      }, {
+        label: '1440x900',
+        value: 65790,
+      }, {
+        label: '1600x900',
+        value: 103410,
+      }, {
+        label: '1680x1050',
+        value: 169550,
+      }, {
+        label: '1920x1080',
+        value: 672440,
+      }, {
+        label: '1920x1200',
+        value: 42560,
+      }, {
+        label: '2560x1440',
+        value: 33170,
+      }, {
+        label: '3840x2160',
+        value: 32360,
+      }
+    ]
+  },
+  physicalDisk: {
+    title: 'Physical disk',
+    dataset: [
+      {
+        label: '<30GB',
+        value: 255272,
+      }, {
+        label: '30-99GB',
+        value: 154609,
+      }, {
+        label: '100-249GB',
+        value: 211407,
+      }, {
+        label: '250-499GB',
+        value: 168307,
+      }, {
+        label: '500-999GB',
+        value: 93231,
+      }, {
+        label: '>1TB',
+        value: 19886,
+      },
+    ]
+  },
+  CPUs: {
+    title: 'Number of CPUs',
+    dataset: [
+      {
+        label: '1-3',
+        value: 1876800,
+      }, {
+        label: '4-6',
+        value: 282800,
+      }, {
+        label: '8+',
+        value: 93200,
+      },
+    ]
+  },
+  RAM: {
+    title: 'Size of RAM (GB)',
+    dataset: [
+      {
+        label: '1-4',
+        value: 1199820,
+      }, {
+        label: '5-8',
+        value: 681390,
+      }, {
+        label: '12-24',
+        value: 305400,
+      }, {
+        label: '32+',
+        value: 61020,
+      },
+    ]
+  },
+  pixelDensity: {
+    title: 'Pixel density',
+    dataset: [
+      {
+        label: '120',
+        value: 413120,
+      }, {
+        label: '160',
+        value: 68110,
+      }, {
+        label: '240+',
+        value: 14560,
+      }, 
+    ]
+  },
+  partitionType: {
+    title: 'Partition type',
+    dataset: [
+      {
+        label: 'Erase existing and reinstall',
+        value: 40,
+      }, {
+        label: 'Manual',
+        value: 30,
+      }, {
+        label: 'Install alongside',
+        value: 17,
+      }, {
+        label: 'Erase device and install',
+        value: 16,
+      }, {
+        label: 'Logical Volume Manager (LVM)',
+        value: 8,
+      }, {
+        label: 'Encrypted LVM',
+        value: 6,
+      }, {
+        label: 'Upgrade',
+        value: 3,
+      }, 
+    ]
+  },
+  partitionSize: {
+    title: 'Size of partitions',
+    dataset: [
+      {
+        label: '<1GB',
+        value: 35,
+      }, {
+        label: '1-19GB',
+        value: 17,
+      }, {
+        label: '20-49GB',
+        value: 14,
+      }, {
+        label: '50-99GB',
+        value: 13,
+      }, {
+        label: '100-249GB',
+        value: 25,
+      }, {
+        label: '250-999GB',
+        value: 20,
+      }, {
+        label: '1TB+',
+        value: 15,
+      },
+    ]
+  },
+  partitionNum: {
+    title: 'Number of partitions',
+    dataset: [
+      {
+        label: '1',
+        value: 36,
+      }, {
+        label: '2',
+        value: 40,
+      }, {
+        label: '3',
+        value: 18,
+      }, {
+        label: '4+',
+        value: 5,
+      },
+    ]
+  },
+  defaultSettings: {
+    dataset: [
+      {
+        label: 'true',
+        value: 310,
+      }, {
+        label: 'false',
+        value: 690,
+      },
+    ]
+  },
+  restrictAddOn: {
+    dataset: [
+      {
+        label: 'true',
+        value: 600,
+      }, {
+        label: 'false',
+        value: 400,
+      },
+    ]
+  },
+  autoLogin: {
+    dataset: [
+      {
+        label: 'true',
+        value: 290,
+      }, {
+        label: 'false',
+        value: 710,
+      },
+    ]
+  },
+  minimalInstall: {
+    dataset: [
+      {
+        label: 'true',
+        value: 130,
+      }, {
+        label: 'false',
+        value: 870,
+      },
+    ]
+  },
+  updateAtInstall: {
+    dataset: [
+      {
+        label: 'true',
+        value: 920,
+      }, {
+        label: 'false',
+        value: 80,
+      },
+    ]
+  },
 };
