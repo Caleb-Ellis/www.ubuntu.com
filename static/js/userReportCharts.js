8f70650e8522612bdbff7f412e5a911da2e0c7e7
Caleb Ellis <caleb.ellis@canonical.com>
2018-08-17 17:56:26 +0100

    wip First pass at building bar charts from dummy data

--- static/js/userReportCharts.js
+++ static/js/userReportCharts.js
@@ -8,8 +8,9 @@ function createPieChart(selector, data, donutRadius, colors) {
   var height = svg.attr('height');
   var width = svg.attr('width');
   var radius = Math.min(height, width) / 2;
-  var g = svg.append("g").attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
   var ordinalColors = d3.scaleOrdinal(colors);
+  var g = svg.append("g")
+    .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
 
   // Generate the pie
   var pie = d3.pie();
@@ -46,9 +47,47 @@ function setInnerHTML(target, value) {
   document.querySelector(target).innerHTML = value;
 }
 
+function createBarChart(selector, data, margin) {
+  var margin = typeof margin !== 'undefined' ? margin : { top: 20, right: 20, bottom: 20, left: 20 };
+
+  var svg = d3.select(selector),
+  width = +svg.attr("width") - margin.left - margin.right,
+  height = +svg.attr("height") - margin.top - margin.bottom,
+  g = svg.append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  var x = d3.scaleBand()
+    .rangeRound([0, width])
+    .padding(0.1);
+
+  var y = d3.scaleLinear()
+    .rangeRound([height, 0]);
+  
+  x.domain(data.dataset.map(function(d) { return d.label }));
+  y.domain([0, d3.max(data.dataset, function(d) { return d.value })]);
+
+  g.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x))
+  
+  g.append("g")
+    .call(d3.axisLeft(y))
+
+  g.selectAll(".bar")
+    .data(data.dataset)
+    .enter()
+    .append("rect")
+    .attr("class", "bar")
+    .attr("x", function(d) { return x(d.label) })
+    .attr("y", function(d) { return y(d.value) })
+    .attr("width", x.bandwidth())
+    .attr("height", function(d) { return height - y(d.value) })
+}
+
 createPieChart('#opt-in', userReportData.optIn, 15);
 createPieChart('#real-or-virtual', userReportData.realOrVirtual, 15);
 createPieChart('#firmware', userReportData.firmware, 15);
+createBarChart('#popular-screen-sizes', userReportData.screenSizes);
 
 setInnerHTML('#os-architecture', userReportData.osArchitecture.dataset[0] + '%');
 setInnerHTML('#display-server', userReportData.displayServer.dataset[0] + '%');
--- static/js/userReportData.js
+++ static/js/userReportData.js
@@ -34,4 +34,66 @@ var userReportData = {
     labels: ['One', 'Two', 'Three'],
     dataset: [87, 10, 3],
   },
+  screenSizes: {
+    title: 'Popular screen sizes',
+    dataset: [
+      {
+        label: '800x600',
+        value: 11.44,
+      }, {
+        label: '1024x768',
+        value: 3.77,
+      }, {
+        label: '1152x768',
+        value: 2.05,
+      }, {
+        label: '1152x864',
+        value: 1.13,
+      }, {
+        label: '1280x1024',
+        value: 3.46,
+      }, {
+        label: '1280x800',
+        value: 2.14,
+      }, {
+        label: '1360x768',
+        value: 1.61,
+      }, {
+        label: '1366x768',
+        value: 25.21,
+      }, {
+        label: '1440x900',
+        value: 2.86,
+      }, {
+        label: '1600x900',
+        value: 4.45,
+      }, {
+        label: '1680x1050',
+        value: 7.57,
+      }, {
+        label: '1920x1080',
+        value: 29.52,
+      }, {
+        label: '1920x1200',
+        value: 1.88,
+      }, {
+        label: '2560x1440',
+        value: 1.48,
+      }, {
+        label: '3840x2160',
+        value: 1.44,
+    }]
+  },
 };
+
+
+
+
+
+
+
+
+
+
+
+
